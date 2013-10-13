fe8bc9bce29a0399f83338fbf5deb8560d9d2a9b
Matt Darnall <mdarnall@salesforce.com>
2013-10-12 20:25:52 -0700

    Adding scheme to shared data

---
+++ GithubTrendingTests/Bar.m
@@ -0,0 +1,34 @@
+//
+//  Bar.m
+//  GithubTrending
+//
+//  Created by Matt Darnall on 10/12/13.
+//  Copyright (c) 2013 Matt Darnall. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Bar : XCTestCase
+
+@end
+
+@implementation Bar
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTAssertTrue(TRUE, @"");
+}
+
+@end
