8eeff065b650e9282e5a6a77f6e311f5fd779da4
Matt Darnall <mdarnall@salesforce.com>
2013-10-12 17:23:35 -0700

    Initial Commit

---
+++ GithubTrending/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GithubTrending
+//
+//  Created by Matt Darnall on 10/12/13.
+//  Copyright (c) 2013 Matt Darnall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GithubTrending/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GithubTrending
+//
+//  Created by Matt Darnall on 10/12/13.
+//  Copyright (c) 2013 Matt Darnall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
