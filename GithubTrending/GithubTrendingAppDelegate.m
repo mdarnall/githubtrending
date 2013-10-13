ab5c5736bc9a681f0e5fca19213fa1ceb0a4bd85
Matt Darnall <mdarnall@salesforce.com>
2013-10-13 15:09:29 -0700

    Adding tests for App Delegate.

--- GithubTrending/GithubTrendingAppDelegate.m
+++ GithubTrending/GithubTrendingAppDelegate.m
@@ -6,9 +6,9 @@
 //  Copyright (c) 2013 Matt Darnall. All rights reserved.
 //
 
-#import "AppDelegate.h"
+#import "GithubTrendingAppDelegate.h"
 
-@implementation AppDelegate
+@implementation GithubTrendingAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
---
+++ GithubTrendingTests/AppDelegateSpec.m
@@ -0,0 +1,43 @@
+    //
+//  AppDelegateSpec.m
+//  GithubTrending
+//
+//  Created by Matt Darnall on 10/12/13.
+//  Copyright (c) 2013 Matt Darnall. All rights reserved.
+//
+
+#import "GithubTrendingAppDelegate.h"
+
+SpecBegin(AppDelegate)
+
+describe(@"AppDelegate", ^{
+
+    __block GithubTrendingAppDelegate *appDelegate;
+
+    beforeAll(^{
+    });
+    
+    beforeEach(^{
+        appDelegate = [[GithubTrendingAppDelegate alloc] init];
+
+    });
+
+    describe(@"application:didFinishLaunchingWithOptions:", ^{
+
+        beforeEach(^{
+            [appDelegate application:nil didFinishLaunchingWithOptions:nil];
+        });
+
+        it(@"should set the root view controller", ^{
+            expect(appDelegate.window.rootViewController).toNot.beNil();
+            expect(appDelegate.window.rootViewController).to.beKindOf([])
+        });
+
+        
+        
+    });
+    
+
+});
+
+SpecEnd
