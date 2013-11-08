c6a3a714f7825f8cf89ab052057ee478956a481a
Matt Darnall <mdarnall@salesforce.com>
2013-11-08 13:06:46 -0800

    Fixing issue where data wasn't set on a re-used table cell

--- GithubTrending/controllers/TrendingTableViewDataSource.m
+++ GithubTrending/controllers/TrendingTableViewDataSource.m
@@ -20,12 +20,12 @@ static NSString *const CellIdentifier = @"TrendingTableViewCell";
     if(cell == nil){
          cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
                                                        reuseIdentifier:CellIdentifier];
-
-
-        NSDictionary * repository = [self.repositories.items objectAtIndex:(NSUInteger) indexPath.row];
-        cell.textLabel.text = [repository objectForKey:@"full_name"];
-        cell.detailTextLabel.text = [repository objectForKey:@"description"];
     }
+
+    NSDictionary * repository = [self.repositories.items objectAtIndex:(NSUInteger) indexPath.row];
+    cell.textLabel.text = [repository objectForKey:@"full_name"];
+    cell.detailTextLabel.text = [repository objectForKey:@"description"];
+
 
     return cell;
 
